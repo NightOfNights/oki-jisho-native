029596e9ddaced55c683715fe07612114796a364
Alexey Panov <lexade1802@gmail.com>
2021-04-02 10:42:31 +0700

    пофиксил logbox для браузера

--- src/App.js
+++ src/App.js
@@ -3,11 +3,15 @@ import RootNavigation from './tabs/rootNavigation';
 import { registerRootComponent } from 'expo';
 import { ThemeProvider } from './providers/themeProvider';
 import { QueryClient, QueryClientProvider } from 'react-query';
-//import { LogBox } from 'react-native';
+import { LogBox } from 'react-native';
 
 const queryClient = new QueryClient();
 
-//LogBox.ignoreLogs(['Setting a timer']);
+try {
+  LogBox.ignoreLogs(['Setting a timer']);
+} catch (e) {
+  console.log(e.message);
+}
 
 const App = () => {
   return (
